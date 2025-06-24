9e59344ab632d74971a43f2c2eda303019dacfd6
WesleySouza13 <souzabusiness13@gmail.com>
2025-06-24 10:00:56 -0300

    config: atualizando requirements

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import pandas as pd
 
 app = FastAPI()
 model = joblib.load('modelo_gradientBoosting.pkl')
 print(type(model))
-class modelinput(BaseModel):
+class ModelInput(BaseModel):
     credit_score: int
     country:str
     gender:str
@@ -21,14 +22,17 @@ class modelinput(BaseModel):
 @app.get('/')
 def home():
     return {'api para previsao de churn - modelo de gradientBoosting. Criado por Wesley Matos no dia 20/06/2025'}
-@app.post('/predict')
-def predict(data: modelinput):
+@app.post('/inference')
+def predict(data: ModelInput):
     if model is None:
         return {'o modelo nao se enconta ou nao foi reconhecido'}
     try:
-        input_array = np.array([[data.credit_score, data.age, data.country, data.gender, data.tenure, data.balance,
-                                data.products_number, data.credit_card, data.active_member, data.estimated_salary, data.level_score]])
-        predict = model.predict(input_array)
+        input = data.model_dump()
+        data_f = pd.DataFrame([input])
+        
+        #input_array = np.array([[data.credit_score, data.age, data.country, data.gender, data.tenure, data.balance,
+                                #data.products_number, data.credit_card, data.active_member, data.estimated_salary, data.level_score]])
+        predict = model.predict(data_f)
         return {f'prediçao: {int(predict[0])}'}
     except Exception as e:
         return {f'ocorreu um erro :{str(e)}'}
